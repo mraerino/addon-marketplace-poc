414dcb65c3509fa6932bab29e0fe89f70d44110e
Marcus Weiner <marcus.weiner@gmail.com>
2019-07-16 16:16:43 +0300

    Fix static build of material ui styles

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-typescript`,
+    `gatsby-plugin-material-ui`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
